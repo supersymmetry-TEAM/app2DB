0fbb3d31efafb8284c709e0da353ee87725862de
SUSY <zx0596@naver.com>
2021-01-07 12:01:23 +0900

    test

--- users/serializers.py
+++ users/serializers.py
@@ -3,6 +3,28 @@ from rest_framework import serializers
 
 
 class UserSerializer(serializers.ModelSerializer):
+
+    password = serializers.CharField(write_only=True)
+
     class Meta:
         model = User
-        fields = '__all__'
+        fields = (
+            "id",
+            "avatar",
+            "first_name",
+            "last_name",
+            "username",
+            "password",
+            'email',
+        )
+        read_only_fields = ("id", "avatar")
+
+    def validate_first_name(self, value):
+        return value.upper()
+
+    def create(self, validated_data):
+        password = validated_data.get("password")
+        user = super().create(validated_data)
+        user.set_password(password)
+        user.save()
+        return user
