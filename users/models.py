e3f2dde1a9dbeefd4a911cb698ab06cbfefac2c4
SUSY <zx0596@naver.com>
2021-01-07 10:36:04 +0900

    test

--- users/models.py
+++ users/models.py
@@ -1,8 +1,8 @@
 from django.contrib.auth.models import AbstractUser
 from django.db import models
+from django.contrib.auth.models import User
 
 
-
-# class User(AbstractUser):
-#     avatar = models.ImageField(upload_to="avatars", blank=True)
-#     # favs = models.ManyToManyField("nutfood.NutData", related_name="favs")
+class User(User):
+    avatar = models.ImageField(upload_to="avatars", blank=True)
+    # favs = models.ManyToManyField("nutfood.NutData", related_name="favs")
--- users/views.py
+++ users/views.py
@@ -12,7 +12,7 @@ from rest_framework import status
 from rest_framework.response import Response
 from rest_framework.permissions import IsAuthenticated
 
-from django.contrib.auth.models import User
+from users.models import User
 from .serializers import UserSerializer
 from .permission import IsSelf
 
