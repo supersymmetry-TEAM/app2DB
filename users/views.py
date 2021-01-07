aeb2408f6d07c3e94ca5cd03f4f173623ecf4863
SUSY <zx0596@naver.com>
2021-01-07 12:12:09 +0900

    test

--- users/views.py
+++ users/views.py
@@ -52,4 +52,4 @@ class UsersViewSet(ModelViewSet):
             )
             return Response(data={"token": encoded_jwt, "id": user.pk})
         else:
-            return Response(data={"user":user, "username":username,"password":password })
+            return Response(status=status.HTTP_401_UNAUTHORIZED)
